f1b2cca28dc0ed47abe91e09059455d1728fceb0
Kyrylo Perekopaiko <kirilperekopayko@gmail.com>
2022-09-21 01:09:50 +0300

    initial commit

--- homework7/index.js
+++ homework7/index.js
@@ -1,6 +1,8 @@
 /*Создать функцию для расчета произведения двух чисел, вызываемого следующим образом: name(5)(2). Функция должна возвращать результат (в середине функции не должно быть консоль лога!)*/
-function calcMultiplication(a,b){
-    return a*b;
+function calcMultiplication(a){
+    return function (b){
+        return a*b;
+    }
 }
-const result = calcMultiplication(2,5);
+const result = calcMultiplication(2)(5);
 console.log(result);
