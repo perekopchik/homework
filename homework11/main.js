95142cc3cd249be9475a34ab219c2429d2565bf5
Kyrylo Perekopaiko <kirilperekopayko@gmail.com>
2022-12-02 17:51:20 +0200

    initial commit

--- homework11/main.js
+++ homework11/main.js
@@ -1,28 +1,42 @@
 /*1) Самостоятельно реализовать пример с урока (todo list).
  2) Добавить метод удаления todo айтема.*/
 document.addEventListener('DOMContentLoaded',function (){
-function Tasks(input, todosWrapper){
-    this.input =document.querySelector(input);
+function Tasks(input, todosWrapper) {
+    this.completeName = 'js--complete';
+    this.deleteName = 'js--delete';
+    this.input = document.querySelector(input);
     this.todosWrapper = document.querySelector(todosWrapper);
     this.addItem = (event) => {
         event.preventDefault();
         const value = this.input.value;
         this.input.value = '';
         const item = this.createTemplate(value);
-        this.todosWrapper.innerHTML = item;
+        this.todosWrapper.insertAdjacentHTML('beforeend', item);
+        document.querySelectorAll(`.${this.completeName}`).forEach(checkboxItem => {
+            checkboxItem.addEventListener('click', this.complete);
+        })
+        document.querySelectorAll(`.${this.deleteName} `).forEach(button => {
+            button.addEventListener('click', this.delete);
+        })
+    }
+    this.complete = function () {
+        this.parentNode.classList.toggle("todo-item__desc--underline");
+    }
+    this.delete = function (){
+        this.parentNode.remove();
     }
     this.createTemplate = (description) => {
-        return(
+        return (
             `       
        <label class="todo-item">
-            <input type="checkbox" class="js--complete">
-            <div class="todo-item__desc">${description}</div>
-            <button class="todo-item__delete js--delete">Удалить</button>
+            <input  class="${this.completeName} todo-item__input" type="checkbox">
+            <div class="todo-item__desc ">${description}</div>
+            <button class="todo-item__delete ${this.deleteName}">Удалить</button>
         </label>
         `
         )
     }
 }
-const task = new Tasks('.js--form','.js--todos-wrapper')
+const task = new Tasks('.form__input','.js--todos-wrapper')
 document.querySelector('.js--form').addEventListener('submit', task.addItem)
 })
