82871bfeea2ce06c9175211c805f8b541c34c4c9
Perekopchik <kirilperekopayko@gmail.com>
2023-01-27 00:03:59 +0200

    initial commit

--- homework13/main.js
+++ homework13/main.js
@@ -1,9 +1,8 @@
 function UserTable({_content, _form, _addButton, _userInfo, _localStorageKeyName}) {
     this.init = function () {
-        this.loadUser();
-        this.console();
         this.onSubmit();
         this.onAddButton();
+        this.loadUser();
     }
     this.onSubmit = function () {
         _form.addEventListener('submit', (event) => {
@@ -24,6 +23,7 @@ function UserTable({_content, _form, _addButton, _userInfo, _localStorageKeyName
         users.push(user);
         localStorage.setItem(_localStorageKeyName, JSON.stringify(users));
     }
+    const _buttonOk = document.querySelector('.js--ok');
     this.userTemplate = function (user) {
         _content.insertAdjacentHTML('beforeend', (
             `<tr data-id="${user.id}">` +
@@ -32,30 +32,60 @@ function UserTable({_content, _form, _addButton, _userInfo, _localStorageKeyName
             `<td>${user.phone}</td>` +
             `<td>${user.age}</td>` +
             `<td>` +
-            '<button type="button" class="js--view">View</button>' +
+            '<button type="button" class="js--view btn">View</button>' +
+            '<button type="button" class="js--delete btn">Delete</button>' +
+            '<button type="button" class="js--edit btn">Edit</button>' +
             `</td>` +
             `</tr>`
         ))
         const _currentTr = document.querySelector(`[data-id="${user.id}"]`);
         const viewButton = _currentTr.querySelector('.js--view');
-        const handleView = () =>{
-            _userInfo.innerHTML= JSON.stringify(user,undefined,2)
+        const deleteButton = _currentTr.querySelector('.js--delete');
+        const editButton = _currentTr.querySelector('.js--edit');
+        editButton.addEventListener('click', function () {
+            _form.classList.add('open');
+            _buttonOk.setAttribute('type', 'button');
+            let input1 = document.querySelector('.mb-1').value = user.name;
+            let input2 = document.querySelector('.mb-2').value = user.phone;
+            let input3 = document.querySelector('.mb-3').value = user.age;
+            _buttonOk.addEventListener('click',function (){
+                users = JSON.parse(localStorage.getItem(_localStorageKeyName));
+                user.name= _form.elements['name'].value;
+                user.phone = _form.elements['age'].value;
+                user.age = _form.elements['phone'].value;
+                localStorage.setItem(_localStorageKeyName, JSON.stringify(users));
+                console.log(input1);
+
+            })
+        })
+        const handleDelete = () => {
+            users = JSON.parse(localStorage.getItem(_localStorageKeyName));
+            for (let n = 0; n < users.length; n++) {
+                if (users[n].id === user.id) {
+                    users.splice(n, 1);
+                    localStorage.setItem(_localStorageKeyName, JSON.stringify(users));
+                    _currentTr.remove();
+                }
+            }
+
         }
-        viewButton.addEventListener('click',handleView)
+        deleteButton.addEventListener('click', handleDelete);
+        const handleView = () => {
+            _userInfo.innerHTML = JSON.stringify(user, undefined, 2)
+        }
+        viewButton.addEventListener('click', handleView)
     }
     this.onAddButton = function () {
         _addButton.addEventListener('click', function () {
+            _buttonOk.setAttribute('type', 'submit');
             _form.classList.add('open');
         })
     }
     this.loadUser = function () {
-        const users = JSON.parse(localStorage.getItem('users'));
+        const users = JSON.parse(localStorage.getItem(_localStorageKeyName));
         if (users) {
             users.forEach(userItem => this.userTemplate(userItem))
         }
-    }
-    this.console = function () {
-        console.log('ya mikola tsap');
     }
 }
 
