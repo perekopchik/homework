044a4039315b8e962a5fe13d0a147d47a70445ab
Perekopchik <kirilperekopayko@gmail.com>
2023-02-08 18:42:43 +0200

    initial commit

--- homework18/main.js
+++ homework18/main.js
@@ -1,11 +1,11 @@
 const response = axios.get('http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19')
 response.then((res)=>{
-    document.querySelector('.q').innerHTML = JSON.stringify(res.data.name);
-    document.querySelector('.temp').innerHTML = JSON.stringify(res.data.main.temp);
-    document.querySelector('.pressure').innerHTML = JSON.stringify(res.data.main.pressure);
-    document.querySelector('.description').innerHTML = JSON.stringify(res.data.weather[0].description);
-    document.querySelector('.humidity').innerHTML = JSON.stringify(res.data.main.humidity);
-    document.querySelector('.speed').innerHTML = JSON.stringify(res.data.wind.speed);
-    document.querySelector('.deg').innerHTML = JSON.stringify(res.data.wind.deg);
-    console.log(res.data);
+    document.querySelector('.q').innerHTML = res.data.name;
+    document.querySelector('.temp').innerHTML = res.data.main.temp;
+    document.querySelector('.pressure').innerHTML = res.data.main.pressure;
+    document.querySelector('.description').innerHTML = res.data.weather[0].description;
+    document.querySelector('.humidity').innerHTML = res.data.main.humidity;
+    document.querySelector('.speed').innerHTML = res.data.wind.speed;
+    document.querySelector('.deg').innerHTML = res.data.wind.deg;
+
 })
