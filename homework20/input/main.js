3307c4483cffa91c9eb61ba98b025c66f4f4b40f
Perekopchik <kirilperekopayko@gmail.com>
2023-02-10 22:34:24 +0200

    initial commit

--- homework20/input/main.js
+++ homework20/input/main.js
@@ -14,22 +14,21 @@ button.addEventListener('click', function () {
                 return String(user.id) === input.value
             })
             const title = document.querySelector('.js--title');
+            const content = document.querySelector('.js--content');
             document.querySelector('.title').insertAdjacentHTML('beforebegin', '<h2>Post</h2>');
             title.innerHTML = user.title;
-            const content = document.querySelector('.js--content');
             content.insertAdjacentHTML('beforebegin',
                 '<button class="showBody">Show</button> ' +
                 '<button class="exit">Exit</button> ');
             document.querySelector('.showBody').addEventListener('click', function () {
                 const body1 = document.querySelector('.body');
+                const body = document.querySelector('.js--body');
+                const email = document.querySelector('.js--email');
+                const name = document.querySelector('.js--name');
                 body1.classList.add('hidden');
                 fetch(`https://jsonplaceholder.typicode.com/comments/${user.id}`)
                     .then(res => res.json())
                     .then(userComment => {
-                        console.log(userComment)
-                        const body = document.querySelector('.js--body');
-                        const email = document.querySelector('.js--email');
-                        const name = document.querySelector('.js--name');
                         document.querySelector('.body').insertAdjacentHTML('beforebegin', `<h3>Comments</h3>`);
                         document.querySelector('.js--name').insertAdjacentHTML('beforebegin', `<h3>Name</h3>`);
                         name.innerHTML = userComment.name;
---
+++ homework21/main.js
@@ -0,0 +1,39 @@
+const button = document.querySelector('.js--button');
+button.addEventListener('click', async function () {
+    const input = document.querySelector('.js--input');
+    if (input.value > 100 || input.value <= 0) {
+        alert('Введите input от 1 до 100');
+        location.reload();
+    }
+    const response = (await fetch('https://jsonplaceholder.typicode.com/posts')).json()
+    const users = await response;
+    const user = users.find((user) => {
+        return String(user.id) === input.value
+    })
+    const title = document.querySelector('.js--title');
+    const content = document.querySelector('.js--content');
+    document.querySelector('.title').insertAdjacentHTML('beforebegin', '<h2>Post</h2>');
+    title.innerHTML = user.title;
+    content.insertAdjacentHTML('beforebegin',
+        '<button class="showBody">Show</button> ' +
+        '<button class="exit">Exit</button> ');
+    document.querySelector('.showBody').addEventListener('click', async function () {
+        const body1 = document.querySelector('.body');
+        const body = document.querySelector('.js--body');
+        const email = document.querySelector('.js--email');
+        const name = document.querySelector('.js--name');
+        body1.classList.add('hidden');
+        const response = (await fetch(`https://jsonplaceholder.typicode.com/comments/${user.id}`)).json()
+        const userComment = await response;
+        document.querySelector('.body').insertAdjacentHTML('beforebegin', `<h3>Comments</h3>`);
+        document.querySelector('.js--name').insertAdjacentHTML('beforebegin', `<h3>Name</h3>`);
+        name.innerHTML = userComment.name;
+        document.querySelector('.js--email').insertAdjacentHTML('beforebegin', `<h3>Email</h3>`);
+        email.innerHTML = userComment.email;
+        document.querySelector('.js--body').insertAdjacentHTML('beforebegin', `<h3>Information</h3>`);
+        body.innerHTML = userComment.body;
+    })
+    document.querySelector('.exit').addEventListener('click', function () {
+        location.reload();
+    })
+})
