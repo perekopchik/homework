232b2589738e3e287e86971a1d871fa4ac136e0c
Perekopchik <kirilperekopayko@gmail.com>
2023-02-10 22:11:33 +0200

    initial commit

--- homework20/input/main.js
+++ homework20/input/main.js
@@ -14,16 +14,30 @@ button.addEventListener('click', function () {
                 return String(user.id) === input.value
             })
             const title = document.querySelector('.js--title');
-            const body = document.querySelector('.js--body');
-            document.querySelector('.title').insertAdjacentHTML('beforebegin', '<h2>Title</h2>');
+            document.querySelector('.title').insertAdjacentHTML('beforebegin', '<h2>Post</h2>');
             title.innerHTML = user.title;
             const content = document.querySelector('.js--content');
             content.insertAdjacentHTML('beforebegin',
                 '<button class="showBody">Show</button> ' +
                 '<button class="exit">Exit</button> ');
             document.querySelector('.showBody').addEventListener('click', function () {
-                document.querySelector('.body').insertAdjacentHTML('beforebegin', '<h3>Body</h3>');
-                body.innerHTML = user.body;
+                const body1 = document.querySelector('.body');
+                body1.classList.add('hidden');
+                fetch(`https://jsonplaceholder.typicode.com/comments/${user.id}`)
+                    .then(res => res.json())
+                    .then(userComment => {
+                        console.log(userComment)
+                        const body = document.querySelector('.js--body');
+                        const email = document.querySelector('.js--email');
+                        const name = document.querySelector('.js--name');
+                        document.querySelector('.body').insertAdjacentHTML('beforebegin', `<h3>Comments</h3>`);
+                        document.querySelector('.js--name').insertAdjacentHTML('beforebegin', `<h3>Name</h3>`);
+                        name.innerHTML = userComment.name;
+                        document.querySelector('.js--email').insertAdjacentHTML('beforebegin', `<h3>Email</h3>`);
+                        email.innerHTML = userComment.email;
+                        document.querySelector('.js--body').insertAdjacentHTML('beforebegin', `<h3>Information</h3>`);
+                        body.innerHTML = userComment.body;
+                    });
             })
             document.querySelector('.exit').addEventListener('click', function () {
                 location.reload();
