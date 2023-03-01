a784f0ceeb6dfe6e84ce9590116b29789abc13d2
Perekopchik <kirilperekopayko@gmail.com>
2023-03-01 22:07:40 +0200

    initial commit

---
+++ homework28/my-app/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import BaseApp from "./containers/BaseApp";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BaseApp/>);
